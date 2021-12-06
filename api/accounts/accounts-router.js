ad58954524a6c9c170ecec30e55ab302c9cbeb1e
Raj-04 <1204patel.raj@gmail.com>
2021-12-06 18:39:31 -0500

    can get the correct number of accounts

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,38 @@
 const router = require('express').Router()
+const {
+  checkAccountId,
+  checkAccountPayload,
+  checkAccountNameUnique,
+} = require('./accounts-middleware')
+const Accounts = require('./accounts-model')
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/', async (req, res, next) => {
+  try {
+    const accounts = await Accounts.getAll()
+    res.status(200).json(accounts)
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  
 })
 
 router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  
 })
 
 router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  
 });
 
 router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
