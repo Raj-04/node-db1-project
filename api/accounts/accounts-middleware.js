3bf295fd442c4d2398464f71f04f2c2b466c3a10
Raj-04 <1204patel.raj@gmail.com>
2021-12-08 11:02:31 -0500

    all tests passing

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,36 +1,55 @@
 const Accounts = require('./accounts-model')
 
 exports.checkAccountPayload = (req, res, next) => {
-  const { name, budget } = req.body
-  if(!name || !budget) {
-    next({
-      status: 400,
-      message: 'name and budget are required'
-    })
-  } else if(typeof name !== 'string') {
-    next({
-      status: 400,
-      message: 'name of account must be a string'
-    })
-  } else if(name.trim().length < 3 || name.trim().length > 100) {
-    next({
-      status: 400,
-      message: 'name of account must be between 3 and 100'
-    })
-  } else if(typeof budget !== 'number' || isNaN(budget)) {
-    next({
-      status: 400,
-      message: 'budget of account must be a number'
-    })
-  } else if(budget < 0 || budget > 1000000) {
-    next({
-      status: 400,
-      message: 'budget of account is too large or too small'
-    })
-  } else if(name && typeof budget === 'number') {
+  // const { name, budget } = req.body
+  // if(!name || !budget) {
+  //   next({
+  //     status: 400,
+  //     message: 'name and budget are required'
+  //   })
+  // } else if(typeof name !== 'string') {
+  //   next({
+  //     status: 400,
+  //     message: 'name of account must be a string'
+  //   })
+  // } else if(name.trim().length < 3 || name.trim().length > 100) {
+  //   next({
+  //     status: 400,
+  //     message: 'name of account must be between 3 and 100'
+  //   })
+  // } else if(typeof budget !== 'number' || isNaN(budget)) {
+  //   next({
+  //     status: 400,
+  //     message: 'budget of account must be a number'
+  //   })
+  // } else if(budget < 0 || budget > 1000000) {
+  //   next({
+  //     status: 400,
+  //     message: 'budget of account is too large or too small'
+  //   })
+  // } else if(name && typeof budget === 'number') {
+  //   req.validatedAccount = {
+  //     ...req.body,
+  //     name: name.trim()
+  //   }
+  //   next()
+  // }
+  if (!req.body.name || req.body.budget === undefined) {
+    next({ status: 400, message: "name and budget are required" });
+  } else if (typeof req.body.name !== "string") {
+    next({ status: 400, message: "name of account must be a string" });
+  } else if (typeof req.body.budget !== "number") {
+    next({ status: 400, message: "budget of account must be a number" });
+  } else if (!req.body.name.trim()) {
+    next({ status: 400, message: "name and budget are required" });
+  } else if (3 > req.body.name.trim().length  || req.body.name.trim().length > 100) {
+    next({ status: 400, message: "name of account must be between 3 and 100" });
+  } else if(req.body.budget < 0 || req.body.budget >1000000){
+    next({ status: 400, message: "budget of account is too large or too small"  });
+  } else if(req.body.name && typeof req.body.budget === 'number') {
     req.validatedAccount = {
       ...req.body,
-      name: name.trim()
+      name: req.body.name.trim()
     }
     next()
   }
